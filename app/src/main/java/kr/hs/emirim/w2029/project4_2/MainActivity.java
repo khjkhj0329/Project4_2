03168bfc675e6901783219739243f5c43d92a01f
효정이 <w2029@e-mirim.hs.kr>
2021-04-15 19:30:22 +0900

    직접풀어보기 4-4 선택완료버튼 없애기

--- app/src/main/java/kr/hs/emirim/w2029/project4_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/project4_2/MainActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.w2029.project4_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.media.session.MediaSessionManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,26 +28,26 @@ public class MainActivity extends AppCompatActivity {
         linear = findViewById(R.id.linear);
         rg = findViewById(R.id.rg);
         imgv = findViewById(R.id.imgv);
-        Button btnDone = findViewById(R.id.btn_done);
+
 
         switchStart.setOnCheckedChangeListener(checkListener);
-        btnDone.setOnClickListener(btnListener);
-
+        rg.setOnCheckedChangeListener(rgListener);
     }
+
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-           if(isChecked) {
-               linear.setVisibility(View.VISIBLE);
-           }else{
-               linear.setVisibility(View.INVISIBLE);
-           }
+            if (isChecked) {
+                linear.setVisibility(View.VISIBLE);
+            } else {
+                linear.setVisibility(View.INVISIBLE);
+            }
         }
     };
-    View.OnClickListener btnListener = new View.OnClickListener() {
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onClick(View v) {
-            switch (rg.getCheckedRadioButtonId()){
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId){
                 case R.id.radio_dog:
                     imgv.setImageResource(R.drawable.dog);
                     break;
